17b542b5aed54d14180ab7b9eccfd7a2da57945c
FlowDetestado <41312326+FlowDetestado@users.noreply.github.com>
2018-08-23 00:23:44 +0100

    Create convite.js

---
+++ commands/convite.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) =>{
+
+   message.delete().catch(O_o=>{})
+   const convite = new Discord.RichEmbed()
+       .setColor("0cff00")
+       .setAuthor("Convite:")
+       
+       .setDescription("Divulgue nosso servidor discord com o seguinte convite: https://discord.gg/GRZ734F !")
+       
+       .setTimestamp()
+       .setFooter(`Moreiraw - BOT`, "https://cdn.discordapp.com/emojis/460264772869554176.gif")
+    
+    message.channel.send(convite)
+
+}
