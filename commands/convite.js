d0dcd87e111009c1ad0e3371761170faaaba09a2
FlowDetestado <41312326+FlowDetestado@users.noreply.github.com>
2018-08-24 19:07:58 +0100

    Update convite.js

--- commands/convite.js
+++ commands/convite.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) =>{
        .setColor("0cff00")
        .setAuthor("Convite:")
        
-       .setDescription("Divulgue nosso servidor discord com o seguinte convite: https://discord.gg/SqgXKCg !")
+       .setDescription("Divulgue nosso servidor discord com o seguinte convite: https://discord.gg/qDqmbyV")
        
        .setTimestamp()
        .setFooter(`Moreiraw - BOT`, "https://cdn.discordapp.com/emojis/460264772869554176.gif")
