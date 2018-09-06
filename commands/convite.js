596463295623f70dc331c480dd5236e2b48b4813
eucomiabacaxi <41312326+eucomiabacaxi@users.noreply.github.com>
2018-09-06 22:31:23 +0100

    Update convite.js

--- commands/convite.js
+++ commands/convite.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) =>{
        .setColor("0cff00")
        .setAuthor("Convite:")
        
-       .setDescription("Divulgue nosso servidor discord com o seguinte convite: https://discord.gg/tTQjxd7")
+       .setDescription("Divulgue nosso servidor discord com o seguinte convite: https://discord.gg/HrW6CGS")
        
        .setTimestamp()
        .setFooter(`Moreiraw - BOT`, "https://cdn.discordapp.com/emojis/460264772869554176.gif")
