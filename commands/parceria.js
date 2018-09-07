4109d3d2eef4960c0a3ca8de003a8cf1d02710f0
eucomiabacaxi <41312326+eucomiabacaxi@users.noreply.github.com>
2018-09-07 20:45:20 +0100

    Update parceria.js

--- commands/parceria.js
+++ commands/parceria.js
@@ -13,7 +13,6 @@ exports.run = (client, message, args) => {
             "embed": {
                 "description": `ㅤㅤㅤㅤㅤㅤㅤㅤ**🙏 Parceria 🙏**ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ\nㅤ**->** ${message.content.replace("!parceria ","")}`,
                 "color": 55512,
-                "footer": "aaaaaa"
                 "thumbnail": {
                     "url": "https://cdn.discordapp.com/attachments/472180056416845824/486995453213147174/sidoka1.png"
                 }
