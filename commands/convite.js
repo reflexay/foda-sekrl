3a9b7a4e6a937c18c8fc28e0b23712066083226d
eucomiabacaxi <41312326+eucomiabacaxi@users.noreply.github.com>
2018-09-12 22:24:33 +0100

    Create convite.js

---
+++ commands/convite.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) =>{
+
+
+   message.delete().catch(O_o=>{})
+   const twitter = new Discord.RichEmbed()
+       .setColor("0cff00")
+       .setAuthor("Convite:")
+       
+       .setDescription("https://discord.gg/f3DxjA2")
+       
+       .setTimestamp()
+       .setFooter(`Moreiraw - BOT`, "https://cdn.discordapp.com/emojis/460264772869554176.gif")
+    
+    message.channel.send(twitter)
+
+}
